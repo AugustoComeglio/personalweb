497723babda08415e8c967c47e7739ac1a12f43c
Augusto Comegllio <augustoc.98@gmail.com>
2024-10-25 20:18:30 -0300

    mmain page

---
+++ src/componets/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import style  from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <div className={style.contenedor}>
+      <p>Augusto Comeglio - 2025</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/componets/Header/Header.tsx
@@ -0,0 +1,11 @@
+import style from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <div className={style.contenedor}>
+      <h1>Augusto Comeglio</h1>
+    </div>
+  )
+}
+
+export default Header
